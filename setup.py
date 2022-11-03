96a978017cae0585ccf93e14212713158f925684
Daniel McKnight <daniel@neon.ai>
2022-11-03 13:59:58 -0700

    Remove unused requirements parsing

--- setup.py
+++ setup.py
@@ -44,24 +44,6 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 
-def get_requirements(requirements_filename: str):
-    requirements_file = path.join(path.abspath(path.dirname(__file__)), "requirements", requirements_filename)
-    with open(requirements_file, 'r', encoding='utf-8') as r:
-        requirements = r.readlines()
-    requirements = [r.strip() for r in requirements if r.strip() and not r.strip().startswith("#")]
-
-    for i in range(0, len(requirements)):
-        r = requirements[i]
-        if "@" in r:
-            parts = [p.lower() if p.strip().startswith("git+http") else p for p in r.split('@')]
-            r = "@".join(parts)
-            if getenv("GITHUB_TOKEN"):
-                if "github.com" in r:
-                    r = r.replace("github.com", f"{getenv('GITHUB_TOKEN')}@github.com")
-            requirements[i] = r
-    return requirements
-
-
 setup(
     name='hardware-components',
     version=version,
@@ -72,7 +54,6 @@ setup(
     description='Hardware Component Classes',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=get_requirements('requirements.txt'),
     packages=find_packages(),
     include_package_data=True
 )
